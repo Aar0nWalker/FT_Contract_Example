3a2f60b8fef22fb87e27812bb9e1cf300a909b52
Aaron Walker <104680900+Aar0nWalker@users.noreply.github.com>
2023-04-19 10:22:25 +0500

    Contract added

---
+++ FT_Example.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable {
+    //Slot 1-4
+    uint256 immutable public _maxTokens = 1000 ether;
+    uint256 public totalTokens;
+    string private _symbol;
+    string private _name;
+
+    constructor() ERC20(_name, _symbol) {
+        _name = "Token";
+        _symbol = "Test";
+        totalTokens = 0;
+    }
+
+    function mint(uint256 amount) public onlyOwner {
+        require(amount + totalTokens <= _maxTokens, "Exceeded max token amount");
+        _mint(msg.sender, amount);
+        totalTokens += amount;
+    }
+
+    function Airdrop(uint256 amount, address to) public onlyOwner {
+        require(amount + totalTokens <= _maxTokens, "Exceeded max token amount");
+        _mint(to, amount);
+        totalTokens += amount;
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        require(amount + totalTokens <= _maxTokens, "Exceeded max token amount");
+        _burn(account, amount);
+        totalTokens -= amount;
+    }
+
+}
