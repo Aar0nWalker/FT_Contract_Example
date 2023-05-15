2d35f4e861b3d7e1e5f2742380cf6a099756d0d2
Aar0nWalker <aaronwalkernft@gmail.com>
2023-05-15 06:02:19 +0500

    Slither audit update

--- FT_Example.sol
+++ FT_Example.sol
@@ -1,19 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./lib/ERC20.sol";
+import "./lib/Ownable.sol";
 
 contract Token is ERC20, Ownable {
-    uint256 immutable public _maxTokens = 1000 ether;
+    uint256 constant public _maxTokens = 1000 ether;
     uint256 public totalTokens;
-    string private _symbol;
-    string private _name;
 
-    constructor() ERC20(_name, _symbol) {
-        _name = "Token";
-        _symbol = "Test";
-        totalTokens = 0;
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        name = "Example";
+        symbol = "FT";
     }
 
     function mint(uint256 amount) public onlyOwner {
